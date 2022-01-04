8d657b96c2e0ca06df925e20e041503585cadf08
blockelgame <69762503+blockelgame@users.noreply.github.com>
2022-01-04 15:59:19 -0600

    Add files via upload

---
+++ scripts/project/scriptsInEvents.js
@@ -0,0 +1,20 @@
+
+
+
+const scriptsInEvents = {
+
+		async Bloxes_jsscriptsheets_Event18_Act2(runtime, localVars)
+		{
+			alert("Game saved")
+			console.log("Grabbing current state of layout")
+			console.log("Making sure it's not mainmenu")
+			console.log("Getting player coords")
+			console.log("Game succesfully saved")
+			console.log("hippity hoppity code go BRRRRRRRRRRRR")
+			console.log("loooooooong boi")
+		}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
